7f027db12cfeba65d3eb798921846d7c889d4ad0
Christine Fredricksen <cmfredricksen@gmail.com>
2022-01-15 20:39:27 -0600

    added text & formatting to about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,38 @@
-<h1>ABout Us</h1>
+<div class="about">
+	<h1>ABout Us</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil veniam nostrum asperiores dolor
+		id ducimus eos cum, atque in ipsam ab ad mollitia adipisci odit reprehenderit fuga soluta,
+		corporis quo sint architecto. Voluptatibus aperiam laboriosam eaque nemo explicabo, eligendi in
+		velit quo. Consectetur earum molestias nemo veniam quia ipsum illo dicta iusto, iste praesentium
+		nam quam vero natus pariatur voluptatum.
+	</p>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil veniam nostrum asperiores dolor
+		id ducimus eos cum, atque in ipsam ab ad mollitia adipisci odit reprehenderit fuga soluta,
+		corporis quo sint architecto. Voluptatibus aperiam laboriosam eaque nemo explicabo, eligendi in
+		velit quo. Consectetur earum molestias nemo veniam quia ipsum illo dicta iusto, iste praesentium
+		nam quam vero natus pariatur voluptatum.
+	</p>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil veniam nostrum asperiores dolor
+		id ducimus eos cum, atque in ipsam ab ad mollitia adipisci odit reprehenderit fuga soluta,
+		corporis quo sint architecto. Voluptatibus aperiam laboriosam eaque nemo explicabo, eligendi in
+		velit quo. Consectetur earum molestias nemo veniam quia ipsum illo dicta iusto, iste praesentium
+		nam quam vero natus pariatur voluptatum.
+	</p>
+</div>
+
+<style>
+	.about {
+		padding: 2rem 1rem 3rem;
+	}
+
+	p,
+	h1 {
+		margin: 1rem 2rem;
+	}
+</style>
